a972ccd06a497aa876808b8251cedb7d7935f9a7
bhoj2056 <bhojraj7709@gmail.com>
2022-12-15 19:28:32 -0800

    Learn about data type

--- helloapp/entry.cs
+++ helloapp/entry.cs
@@ -1,7 +1,48 @@
-﻿ class entry
- {
-  public static void Main()
-  {
-    Console.WriteLine("Hello");
-  }  
- }
+﻿
+
+// using System;
+// class entry
+// {
+//     public static void Main()
+//     {
+//         // Number: integers and floating point
+//         // integer
+//         byte a = 12;
+//         short b = 2350;
+//         int c = 23456;
+//         long d = 223455555;
+//         //floating 
+//         float e = 234.59545677655f;
+//         double f = 3456.456;
+//         decimal g = 2345675.45345544566m;
+
+//         //Text
+//         string name = "Bhoj joshi";
+//         char gender = 'm';
+//         //Boolean
+//         bool areYouMale = true;
+//         Console.WriteLine("the integer value test=" + a);
+//         Console.WriteLine(b);
+//         Console.WriteLine(c);
+//         Console.WriteLine("Byte=" + d);
+//         Console.WriteLine("Byte=" + a);
+//         Console.WriteLine("Byte=" + a);
+//         Console.WriteLine("Byte=" + a);
+
+
+class test
+{
+    public static void Main()
+    {
+        var Number = 89;
+        if (Number % 2 == 0)
+        {
+            Console.WriteLine(Number + " is even");
+        }
+        else
+        {
+            Console.WriteLine(Number + " is odd");
+        }
+    }
+}
+
