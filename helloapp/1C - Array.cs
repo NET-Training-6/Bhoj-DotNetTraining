a6474971b93815b90fae34bd85936e2843fb933e
bhoj2056 <bhojraj7709@gmail.com>
2022-12-29 13:24:22 +0545

    sensible commit message

---
+++ helloapp/1C - Array.cs
@@ -0,0 +1,16 @@
+class ArrayLearner
+{
+    
+    public static void Main()
+    {
+
+               
+        string[] names = {"Hari","Ram","Shyam","Gopal","Suraj","Prakesh","Bhoj","Bikash","Subas","Paras"};
+        for (int i = 0; i < names.Length; i++)
+        {
+            Console.WriteLine(names[i]);
+           
+        }
+       
+    }
+}
--- helloapp/entry.cs
+++ helloapp/entry.cs
@@ -1,124 +1,13 @@
 ﻿
-
-// using System;
-//
-//         // Number: integers and floating point
-//         // integer
-//         byte a = 12;
-//         short b = 2350;
-//         int c = 23456;
-//         long d = 223455555;
-//         //floating 
-//         float e = 234.59545677655f;
-//         double f = 3456.456;
-//         decimal g = 2345675.45345544566m;
-
-//         //Text
-//         string name = "Bhoj joshi";
-//         char gender = 'm';
-//         //Boolean
-//         bool areYouMale = true;
-//         Console.WriteLine("the integer value test=" + a);
-//         Console.WriteLine(b);
-//         Console.WriteLine(c);
-//         Console.WriteLine("Byte=" + d);
-//         Console.WriteLine("Byte=" + a);
-//         Console.WriteLine("Byte=" + a);
-//         Console.WriteLine("Byte=" + a);
-
-//    var input = Console.ReadLine();
-//     var Number = int.par;
-//     if (Number % 2 == 0)
-//     {
-//         Console.WriteLine(Number + " is even");
-//     }
-//     else
-//     {
-//         Console.WriteLine(Number + " is odd");
-//     } 
-//  Console.Write("Enter an amount:");
-// int amt = int.Parse(Console.ReadLine());
-// Console.Write ("Enter a Time period:");
-// int ti = int.Parse(Console.ReadLine());
-// Console.Write("Enter interest:");
-// int rate = int.Parse(Console.ReadLine());
-// int si= amt*ti*rate/100;
-// Console.WriteLine("the simple interest is="+si); 
-// find 
-// class test
-// {
-//     public static void Main()
-//     {
-//     int num;
-//         Console.WriteLine("Enter your check number:");
-//         num=Convert.ToInt32(Console.ReadLine());
-//         if (num %3==0 && num%5==0)
-//         {
-//             Console.WriteLine(num +" is even");
-//             Console.WriteLine(num+" is a multiple of 3 and 5");
-//         }
-//         else if(num%5==0)
-//         {
-//             Console.WriteLine(num+" is multiple of 5");
-//         }
-//         else if(num%3==0)
-//         {
-//             Console.WriteLine(num+" is multiple of 3");
-//         }
-//         else
-//         {
-//                 Console.WriteLine(num+" is odd");
-//             Console.WriteLine(num+" is not multiple of 3 nor 5");
-//         }
-
-//     }
-
-
-// }
 class entry
 {
-    public static void Main()
+    public static void Main1()
     {
-        // Console.Write("principle:");
-        // var pText = Console.ReadLine();
-        // var p = double.Parse(pText);
+pattern p = new pattern();
+p.IncPattern();
+p.DecPattern();
+p.HashPattern();
 
-        // Console.Write("Years):");
-        // var tText = Console.ReadLine();
-        // var t = double.Parse(tText);
-
-        // Console.Write("Rate(%):");
-        // var rText = Console.ReadLine();
-        // var r = double.Parse(rText);
-        // var c= p * Math.Pow((1 + r / 100), t) - p;
-        // // Console.WriteLine("compond Interest of " + p + "for" + t + )
-        // Console.WriteLine($"Cmpound Interest of {p} for {t} years at {r}% is {ci}");
-        // Console.ReadKey();
-
-        // person person1 = new();
-        // person1.Name = "Bhoj joshi";
-        // person1.age = 22;
-        // person person2 = new();
-        // person person3 = new();
-        // person person4 = new();
-        // person person5 = new();
-        // person person6 = new();
-        // person person7 = new();
-        // person person8 = new();
-
-        Animal animal1 = new Animal();
-        animal1.petName = "tiger";
-        animal1.color = " blaack strips ";
-        animal1.height = 5;
-        animal1.weight = 5;
-        animal1.iswild = true;
-        
-
-
-   
-
-
-
- }
+    }
 }
 
