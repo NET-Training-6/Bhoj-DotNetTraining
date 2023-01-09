4fa0c3d3a2c43026e21b733b9fb1a7d47da90984
bhoj2056 <bhojraj7709@gmail.com>
2023-01-09 22:00:45 +0545

    sensible commit message

--- helloapp/ClassAndObjects.cs
+++ helloapp/ClassAndObjects.cs
@@ -1,35 +1,35 @@
-// // class person
-// // {
-// //     // members (methods, fields
-// //     //
-// //     public string Name;
-// //    public byte age;
-// //    public string address;
-// //    public  float height;
-// //   public  DateTime dob;
-// //   public  void calculateBIM()
-// //     {
-
-// //     }
-// // }
-
-// // class SchoolMonitor // pascal casing: class and fiend
-
-// class Animal
+// class person
 // {
-//     public string petName;
-//     public string color;
-//     public float height;
-//     public float weight;
-//     public Boolean iswild;
+//     // members (methods, fields
+//     //
+//     public string Name;
+//    public byte age;
+//    public string address;
+//    public  float height;
+//   public  DateTime dob;
+//   public  void calculateBIM()
+//     {
 
-//     public static void Main(){
+//     }
+// }
 
-//         Animal a1 = new Animal();
-//         a1.petName ="dog";
+// class SchoolMonitor // pascal casing: class and fiend
+
+class Animal
+{
+    public string petName;
+    public string color;
+    public float height;
+    public float weight;
+    public Boolean iswild;
+
+    public static void Main(){
+
+        Animal a1 = new Animal();
+        a1.petName ="dog";
         
 
-// }
+}
 
-// }
+}
 
